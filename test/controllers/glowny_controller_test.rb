0c6cf7f39759a38d1f95b148b7802c0b6a8a0587
Pawel <psielachowicz@gmail.com>
2014-08-25 02:22:36 +0200

    q

---
+++ test/controllers/glowny_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class GlownyControllerTest < ActionController::TestCase
+  test "should get idx" do
+    get :idx
+    assert_response :success
+  end
+
+end
---
+++ test/helpers/glowny_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class GlownyHelperTest < ActionView::TestCase
+end
