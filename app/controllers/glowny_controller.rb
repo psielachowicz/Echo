2b19a1efdd709c78859a01e7a6a5c410e01d12f9
Pawel <psielachowicz@gmail.com>
2014-08-25 02:21:21 +0200

    q

---
+++ app/controllers/glowny_controller.rb
@@ -0,0 +1,12 @@
+class GlownyController < ApplicationController
+  def idx
+  Echowrap.configure do |config|
+  config.api_key =       'NXJ0LVECMNCFTEZ07 '
+  config.consumer_key =  '38f5a614495468b0c0c99d2df4b732f2'
+  config.shared_secret = 'KoQO6dO+SEqJhwTG3FcF3A'
+end
+  @artist = Echowrap.artist_search(:name => params[:artist_name], 
+                                     :results => 1, 
+                                     :bucket => ['hotttnesss', 'familiarity', 'artist_location'])
+  end
+end
---
+++ app/helpers/glowny_helper.rb
@@ -0,0 +1,2 @@
+module GlownyHelper
+end
