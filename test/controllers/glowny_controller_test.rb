e3c9622c3c672b7ba791eb43fbd1f7ac67aa42a9
Pawel <psielachowicz@gmail.com>
2014-08-26 02:16:40 +0200

    pppo

--- test/controllers/glowny_controller_test.rb
+++ test/controllers/glowny_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
-describe glowny_controller do
-
-before do
-    Echowrap.artist_biographies(:id => 'ARH6W4X1187B99274F', :results => 3)
+class GlownyControllerTest < ActionController::TestCase
+  test "should get idx" do
+    get :idx
+    assert_response :success
+    
   end
 
 
